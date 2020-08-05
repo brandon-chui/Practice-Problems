95bad556d4e2cc41d2de3a7ae863051c09d6f3a9
Brandon Chui <boiboy325@gmail.com>
2020-08-05 14:52:35 -0700

    changes to debounce function

--- interview/debounce.js
+++ interview/debounce.js
@@ -1,27 +1,27 @@
 // debounce function delays the function until the last event/function is called 
 
 function debounce(fn, delay) {
-  let timer;
-  let first = true; // make sure the first function always runs
-  return function(...args) {
-    if(first) {
-        setTimeout(() => {
-        fn(...args)
-        first = true; // make sure if function has run, it resets first to true
-      }, 0)
-      first = false;
-    } else {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-          fn(...args)
-          first = true;
-        }, delay)
+    let timer;
+    let first = true; // make sure the first function always runs
+    return function (...args) {
+        if (first) {
+            setTimeout(() => {
+                fn(...args)
+                first = true; // make sure if function has run, it resets first to true
+            }, 0)
+            first = false;
+        } else {
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                fn(...args)
+                first = true;
+            }, delay)
+        }
     }
-  }
 }
 
 function sayHello(name) {
-  console.log('hello', name);
+    console.log('hello', name);
 }
 
 let debouceSayHello = debounce(sayHello, 1000)
@@ -29,9 +29,9 @@ let debouceSayHello = debounce(sayHello, 1000)
 debouceSayHello('brandon')
 
 setTimeout(() => {
-  debouceSayHello('brandon')
-  debouceSayHello('brandon1')
-  debouceSayHello('brandon2')
-  debouceSayHello('brandon3')
-  debouceSayHello('brandon4')
+    debouceSayHello('brandon')
+    debouceSayHello('brandon1')
+    debouceSayHello('brandon2')
+    debouceSayHello('brandon3')
+    debouceSayHello('brandon4')
 }, 3000)
